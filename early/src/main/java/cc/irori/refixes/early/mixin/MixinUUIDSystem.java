75b08cb7c5c2cb00cfafac28de62d00025f9f3f0
KabanFriends <yuukun2004@protonmail.ch>
2026-02-15 18:29:13 +0900

    refactor: do not use Shift.BY in MixinUUIDSystem

--- early/src/main/java/cc/irori/refixes/early/mixin/MixinUUIDSystem.java
+++ early/src/main/java/cc/irori/refixes/early/mixin/MixinUUIDSystem.java
@@ -1,18 +1,15 @@
 package cc.irori.refixes.early.mixin;
 
 import cc.irori.refixes.early.util.Logs;
-import com.hypixel.hytale.component.CommandBuffer;
-import com.hypixel.hytale.component.Ref;
-import com.hypixel.hytale.component.RemoveReason;
-import com.hypixel.hytale.component.Store;
+import com.hypixel.hytale.component.*;
 import com.hypixel.hytale.logger.HytaleLogger;
 import com.hypixel.hytale.server.core.entity.UUIDComponent;
 import com.hypixel.hytale.server.core.universe.world.storage.EntityStore;
-import com.llamalad7.mixinextras.sugar.Local;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.Redirect;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(EntityStore.UUIDSystem.class)
@@ -21,6 +18,24 @@ public class MixinUUIDSystem {
     @Unique
     private static final HytaleLogger refixes$LOGGER = Logs.logger();
 
+    @Unique
+    private static final ThreadLocal<UUIDComponent> refixes$UUID_COMPONENT = new ThreadLocal<>();
+
+    @SuppressWarnings("unchecked")
+    @Redirect(
+            method = "onEntityRemove",
+            at =
+                    @At(
+                            value = "INVOKE",
+                            target =
+                                    "Lcom/hypixel/hytale/component/CommandBuffer;getComponent(Lcom/hypixel/hytale/component/Ref;Lcom/hypixel/hytale/component/ComponentType;)Lcom/hypixel/hytale/component/Component;"))
+    private <T extends Component<EntityStore>> T refixes$storeUuidComponent(
+            CommandBuffer<EntityStore> instance, Ref<EntityStore> ref, ComponentType<EntityStore, ?> componentType) {
+        UUIDComponent uuidComponent = (UUIDComponent) instance.getComponent(ref, componentType);
+        refixes$UUID_COMPONENT.set(uuidComponent);
+        return (T) uuidComponent;
+    }
+
     @Inject(
             method = "onEntityRemove",
             at =
@@ -28,19 +43,19 @@ public class MixinUUIDSystem {
                             value = "INVOKE",
                             target =
                                     "Lcom/hypixel/hytale/component/CommandBuffer;getComponent(Lcom/hypixel/hytale/component/Ref;Lcom/hypixel/hytale/component/ComponentType;)Lcom/hypixel/hytale/component/Component;",
-                            shift = At.Shift.BY,
-                            by = 3),
+                            shift = At.Shift.AFTER),
             cancellable = true)
     private void refixes$validateUuidComponent(
             Ref<EntityStore> ref,
             RemoveReason reason,
             Store<EntityStore> store,
             CommandBuffer<EntityStore> commandBuffer,
-            CallbackInfo ci,
-            @Local(name = "uuidComponent") UUIDComponent uuidComponent) {
+            CallbackInfo ci) {
+        UUIDComponent uuidComponent = refixes$UUID_COMPONENT.get();
         if (uuidComponent == null) {
             refixes$LOGGER.atWarning().log("UUIDSystem#onEntityRemove(): UUIDComponent is null");
             ci.cancel();
         }
+        refixes$UUID_COMPONENT.remove();
     }
 }
