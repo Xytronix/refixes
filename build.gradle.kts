9b98dac9d1a1d6b19723a0be62fae9cf0ee189f8
KabanFriends <yuukun2004@protonmail.ch>
2026-02-17 03:06:55 +0900

    build: bundle generator task

--- build.gradle.kts
+++ build.gradle.kts
@@ -10,3 +10,47 @@ allprojects {
     group = modGroup
     version = modVersion
 }
+
+tasks {
+    register<Delete>("cleanBundle") {
+        delete(layout.buildDirectory.dir("bundle"))
+    }
+
+    register<Copy>("copyBundleManifest") {
+        dependsOn("cleanBundle")
+        from("bundle") {
+            include(
+                "manifest.json",
+                "LICENSE",
+                "README.md"
+            )
+        }
+        into(layout.buildDirectory.dir("bundle"))
+        expand(
+            "version" to modVersion,
+            "hytaleVersion" to libs.versions.hytale.get()
+        )
+        duplicatesStrategy = DuplicatesStrategy.INCLUDE
+    }
+
+    register<Copy>("collectEarlyJar") {
+        dependsOn("copyBundleManifest")
+        val task = project(":early").tasks.named("shadowJar")
+        from(task)
+        into(layout.buildDirectory.dir("bundle/earlyplugins"))
+    }
+
+    register<Copy>("collectMainJar") {
+        dependsOn("copyBundleManifest")
+        val task = project(":plugin").tasks.named("shadowJar")
+        from(task)
+        into(layout.buildDirectory.dir("bundle/mods"))
+    }
+
+    register<Zip>("bundle") {
+        dependsOn("collectEarlyJar", "collectMainJar")
+        archiveFileName.set("${project.name}-bundle-${version}.zip")
+        destinationDirectory.set(file("bundle"))
+        from(layout.buildDirectory.dir("bundle"))
+    }
+}
