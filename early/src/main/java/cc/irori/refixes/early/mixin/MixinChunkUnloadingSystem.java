d0a58b4baa9e3f62f8629f4d98fae0d65fc15997
Xytronix <32957125+Xytronix@users.noreply.github.com>
2026-03-14 15:29:04 +0100

    fix build issue

--- early/src/main/java/cc/irori/refixes/early/mixin/MixinChunkUnloadingSystem.java
+++ early/src/main/java/cc/irori/refixes/early/mixin/MixinChunkUnloadingSystem.java
@@ -1,52 +1,20 @@
 package cc.irori.refixes.early.mixin;
 
-import cc.irori.refixes.early.EarlyOptions;
+import java.util.List;
+
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Overwrite;
+
 import com.hypixel.hytale.component.ArchetypeChunk;
 import com.hypixel.hytale.component.CommandBuffer;
 import com.hypixel.hytale.component.Store;
-import com.hypixel.hytale.math.util.ChunkUtil;
 import com.hypixel.hytale.server.core.modules.entity.player.ChunkTracker;
-import com.hypixel.hytale.server.core.universe.world.World;
-import com.hypixel.hytale.server.core.universe.world.chunk.WorldChunk;
 import com.hypixel.hytale.server.core.universe.world.storage.ChunkStore;
 import com.hypixel.hytale.server.core.universe.world.storage.EntityStore;
-import java.util.List;
-import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Overwrite;
-import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 // Fixes vanilla bug where paused players' chunks unload after 7.5 seconds
 @Mixin(targets = "com.hypixel.hytale.server.core.universe.world.storage.component.ChunkUnloadingSystem")
 public class MixinChunkUnloadingSystem {
-
-    @Inject(
-            method = "tryUnload",
-            at = @At(value = "INVOKE", target = "Lcom/hypixel/hytale/server/core/universe/world/chunk/WorldChunk;pollKeepAlive(I)I"),
-            cancellable = true)
-    private static void refixes$protectSpawnChunk(
-            int index,
-            ArchetypeChunk<ChunkStore> archetypeChunk,
-            CommandBuffer<ChunkStore> commandBuffer,
-            CallbackInfo ci) {
-        if (!EarlyOptions.isAvailable() || !EarlyOptions.VANILLA_KEEP_SPAWN_LOADED.get()) {
-            return;
-        }
-
-        WorldChunk worldChunk = archetypeChunk.getComponent(index, WorldChunk.getComponentType());
-        World world = ((ChunkStore) commandBuffer.getStore().getExternalData()).getWorld();
-
-        int spawnX = (int) world.getWorldConfig().getSpawnPosition().getX();
-        int spawnZ = (int) world.getWorldConfig().getSpawnPosition().getZ();
-        int spawnChunkX = ChunkUtil.chunkCoordinate(spawnX);
-        int spawnChunkZ = ChunkUtil.chunkCoordinate(spawnZ);
-
-        if (worldChunk.getX() == spawnChunkX && worldChunk.getZ() == spawnChunkZ) {
-            worldChunk.resetKeepAlive();
-            ci.cancel();
-        }
-    }
 
     @Overwrite
     private static void collectTrackers(
@@ -57,7 +25,7 @@ public class MixinChunkUnloadingSystem {
 
         for (int index = 0; index < archetypeChunk.size(); ++index) {
             ChunkTracker chunkTracker = archetypeChunk.getComponent(index, ChunkTracker.getComponentType());
-            if (chunkTracker != null && chunkTracker.shouldBeVisible(chunkTracker.getChunkIndex())) {
+            if (chunkTracker != null && ((ChunkTrackerAccessor) chunkTracker).getTransformComponent() != null) {
                 dataResource.getChunkTrackers().add(chunkTracker);
             }
         }
@@ -69,6 +37,11 @@ public class MixinChunkUnloadingSystem {
     interface DataAccessor {
         List<ChunkTracker> getChunkTrackers();
     }
-}
 
+    @Mixin(ChunkTracker.class)
+    interface ChunkTrackerAccessor {
+        @org.spongepowered.asm.mixin.gen.Accessor("transformComponent")
+        Object getTransformComponent();
+    }
+}
 
